e33272f16859137c44aa0377d3c85a1d20e0ad07
Ralf <159915551+ralf-devop@users.noreply.github.com>
2026-07-22 20:00:13 +0200

    close #28: Simulated training env has been improved

--- projects/terra-incognita/README.md
+++ projects/terra-incognita/README.md
@@ -27,6 +27,12 @@ terraform apply
 #with your own values for variables 
 terraform apply -var 'number_trainees=5'
 ```
+Trainer and trainees keys for SSH connection are created in the project folder
+
+**Connect as trainer to the created EC2 instance**
+```
+ssh -i ./trainer-ssh-key ec2-user@15.183.53.14
+```
 
 **Connect as trainee to the created EC2 instance**
 ```
@@ -35,5 +41,6 @@ ssh -i ./training-ssh-key trainee_0@15.183.53.14
 
 **Delete previously created resources**
 ```
+#this will also remove the keys for the SSH connection
 terraform destroy
 ```
--- projects/terra-incognita/main.tf
+++ projects/terra-incognita/main.tf
@@ -3,6 +3,31 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+##########################
+# Trainer - AWS Key Pair #
+##########################
+
+# Create private key for trainer ssh connection
+resource "tls_private_key" "trainer_key" {
+  algorithm = "ED25519"
+}
+
+# Create AWS key pair for trainer
+resource "aws_key_pair" "trainer_key_pair" {
+  key_name   = "trainer-ssh-key"
+  public_key = tls_private_key.trainer_key.public_key_openssh
+}
+
+# Write the private key to the hard drive so that the trainer can connect later
+resource "local_sensitive_file" "local_trainer_key" {
+  content  = tls_private_key.trainer_key.private_key_openssh
+  filename = "${path.module}/${aws_key_pair.trainer_key_pair.key_name}"
+}
+
+##########################
+# Trainee - AWS Key Pair #
+##########################
+
 # Create private key for ssh connection
 resource "tls_private_key" "training_key" {
   algorithm = "ED25519"
@@ -20,14 +45,19 @@ resource "local_sensitive_file" "local_training_key" {
   filename = "${path.module}/${aws_key_pair.training_key_pair.key_name}"
 }
 
+#####################
+# AWS EC2 Instances #
+#####################
+
 # Create own training environment for each trainee
 resource "aws_instance" "training-env" {
   ami = "ami-0de6934e87badb694"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.training_key_pair.key_name
+  key_name = aws_key_pair.trainer_key_pair.key_name
   count = "${var.number_trainees}"
 
   user_data = templatefile("${path.module}/user_data.sh.tpl", {
+    packages = ["java-25-amazon-corretto", "java-25-amazon-corretto-devel"]
     key = aws_key_pair.training_key_pair.public_key
     key_name = "${aws_key_pair.training_key_pair.key_name}"
     user = "trainee_${count.index}"
@@ -44,7 +74,8 @@ output "trainee_public_ip" {
     for k, v in aws_instance.training-env : k => {
       ip = v.public_ip
       user = "trainee_${k}"
-      connection_string = "ssh -i ${aws_key_pair.training_key_pair.key_name} trainee_${k}@${v.public_ip}"
+      ssh_command_trainer = "ssh -i ${aws_key_pair.trainer_key_pair.key_name} ec2-user@${v.public_ip}"
+      ssh_command_trainee = "ssh -i ${aws_key_pair.training_key_pair.key_name} trainee_${k}@${v.public_ip}"
     }
   }
 }
